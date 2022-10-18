f8b8581747226d84a779537e33f5094e649fe3ce
gshduet <modis2199@gmail.com>
2022-10-18 17:13:09 +0900

    Feat: User Modeling

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,8 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-
+    'users',
+    
 ]
 
 INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
@@ -134,6 +135,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.user'
 
 LOGGING = {
     'disable_existing_loggers': False,
